ad640e5c94fcb87b3ded9219e401de5306754f74
Tomasz Wychocki <wychoniu@gmail.com>
2017-08-27 21:17:37 +0200

    New lib. Ball effect

--- ArduinoMatrixController.ino
+++ ArduinoMatrixController.ino
@@ -1,4 +1,4 @@
-#include "LedControl.h"
+#include <LEDMatrixDriver.hpp>
 #define MODULES_X 5
 #define MODULES_Y 2
 #define MODULES_COUNT MODULES_X*MODULES_Y
@@ -6,7 +6,7 @@
 #define PIXELS_Y MODULES_Y*8
 
 bool FrameBuffer[PIXELS_Y][PIXELS_X];
-LedControl lc = LedControl(12, 11, 10, MODULES_COUNT);
+LEDMatrixDriver lmd(MODULES_COUNT, 9); //CS MOSI=DIN
 
 byte character[200][5];
 
@@ -17,36 +17,58 @@ void clearBuffer() {
 }
 
 void setup() {
-  for (int i = 0; i < MODULES_COUNT; i++) {
-    lc.shutdown(i, false);
-    lc.setIntensity(i, 8);
-    lc.clearDisplay(i);
-  }
+  lmd.setEnabled(true);
+  lmd.setIntensity(2);   // 0 = low, 10 = high
   clearBuffer();
 }
 
-byte boolToByte(int startY, int startX) {
-  byte b = 0;
-  for (int i = 0; i < 8; i++)
-    b |= FrameBuffer[startY][startX + i] << (7 - i);
-  return b;
-}
-
 void redisplay() {
-  int module;
-  for (int i = 0; i < PIXELS_Y; i++) {
-    for (int j = 0; j < PIXELS_X; j += 8) {
-      module = (j / 8) + (5 * (i / 8));
-      lc.setRow(module, i % 8, boolToByte(i, j));
+  int x=40, y=0;
+  
+  for (int i = 0; i < PIXELS_Y/2; i++) {
+    for (int j = 0; j < PIXELS_X; j++) {
+      lmd.setPixel(x++,y,FrameBuffer[i][j]);
+      if(x >= 80) {
+        x = 40;
+        y++;  
+      }
     }
   }
+
+  x=0; y=0;
+  
+  for (int i = PIXELS_Y/2; i < PIXELS_Y; i++) {
+    for (int j = 0; j < PIXELS_X; j++) {
+      lmd.setPixel(x++,y,FrameBuffer[i][j]);
+      if(x >= 40) {
+        x = 0;
+        y++;  
+      }
+    }
+  }
+
+  lmd.display();
 }
 
 void draw5x4(int posY, int posX, char c) {
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+int dx = 1, dy = -1;
+int posx = 6, posy = 10;
 
+void loop() {
+  FrameBuffer[posy][posx] = false;
+
+  posy+=dy;
+  posx+=dx;
+  FrameBuffer[posy][posx] = true;
+
+  if(posy <= 0) dy = 1;
+  if(posy >= 15) dy = -1;
+  if(posx <= 0) dx = 1;
+  if(posx >= 39) dx = -1;
+
+  redisplay();
+  delay(10);
 }
