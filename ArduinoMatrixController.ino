2e7f4c83616ec6e1772e42cf54f1676d65c0a4f7
Tomasz Wychocki <wychoniu@gmail.com>
2017-08-30 21:07:17 +0200

    Simple web server

--- ArduinoMatrixController.ino
+++ ArduinoMatrixController.ino
@@ -1,16 +1,22 @@
 #include <LEDMatrixDriver.hpp>
+#include <EEPROM.h>
 #include "font.h"
-//#include "wifi.h"
 #define MODULES_X 5
 #define MODULES_Y 2
 #define MODULES_COUNT MODULES_X*MODULES_Y
 #define PIXELS_X MODULES_X*8
 #define PIXELS_Y MODULES_Y*8
 
+void displayText(String);
+/*
+  EEPROM:
+  0 - LED Intensity
+*/
+
 bool FrameBuffer[PIXELS_Y][PIXELS_X];
 LEDMatrixDriver lmd(MODULES_COUNT, 15); //CS MOSI=DIN
 long int lastSecond = 0;
-int g = 0, m = 0, s = 0;
+#include "wifi.h"
 
 void clearBuffer() {
   for (int i = 0; i < PIXELS_Y; i++)
@@ -19,37 +25,48 @@ void clearBuffer() {
 }
 
 void setup() {
+  EEPROM.begin(512);
   lmd.setEnabled(true);
-  lmd.setIntensity(0);   // 0 = low, 10 = high
+  lmd.setIntensity(EEPROM.read(0));   // 0 = low, 10 = high
   clearBuffer();
-  draw8x16(0,0,g/10);
-  draw8x16(0,9,g%10);
-  draw8x16(0,23,m/10);
-  draw8x16(0,32,m%10);
+  draw8x16(0, 0, g / 10);
+  draw8x16(0, 9, g % 10);
+  draw8x16(0, 23, m / 10);
+  draw8x16(0, 32, m % 10);
   redisplay();
+
+  //displayText("Tomasz Wychocki");
+
+  Serial.begin(115200);
+  connectWiFi();
+
+  server.on("/", handleRoot);
+  server.begin();
+  
+  getNTPTime();
 }
 
 void redisplay() {
-  int x=40, y=0;
-  
-  for (int i = 0; i < PIXELS_Y/2; i++) {
+  int x = 40, y = 0;
+
+  for (int i = 0; i < PIXELS_Y / 2; i++) {
     for (int j = 0; j < PIXELS_X; j++) {
-      lmd.setPixel(x++,y,FrameBuffer[i][j]);
-      if(x >= 80) {
+      lmd.setPixel(x++, y, FrameBuffer[i][j]);
+      if (x >= 80) {
         x = 40;
-        y++;  
+        y++;
       }
     }
   }
 
-  x=0; y=0;
-  
-  for (int i = PIXELS_Y/2; i < PIXELS_Y; i++) {
+  x = 0; y = 0;
+
+  for (int i = PIXELS_Y / 2; i < PIXELS_Y; i++) {
     for (int j = 0; j < PIXELS_X; j++) {
-      lmd.setPixel(x++,y,FrameBuffer[i][j]);
-      if(x >= 40) {
+      lmd.setPixel(x++, y, FrameBuffer[i][j]);
+      if (x >= 40) {
         x = 0;
-        y++;  
+        y++;
       }
     }
   }
@@ -58,32 +75,88 @@ void redisplay() {
 }
 
 void draw8x16(int posY, int posX, int number) {
-  for(int i=0; i<16; i++) {
-    for(int j=0; j<8; j++) {
-        FrameBuffer[posY+i][posX+j] = font8x16[number][i] & (1 << (7-j));
-    }  
+  for (int i = 0; i < 16; i++) {
+    for (int j = 0; j < 8; j++) {
+      FrameBuffer[posY + i][posX + j] = font8x16[number][i] & (1 << (7 - j));
+    }
   }
 }
 
-void loop() {
-  //if(millis()-last>=1800000) { getNTPTime(); }
+void displayText(String text) {
+  bool **a;
+  a = (bool **) malloc(7 * sizeof(bool *));
+  for (int i = 0; i < 7; i++)
+    a[i] = (bool *) malloc(6 * text.length() * sizeof(bool));
 
-  if(millis()-lastSecond>=1000) { 
-    s++; 
-    lastSecond = millis(); 
+  clearBuffer();
+  int posx = 0;
+
+  for (int c = 0; c < text.length(); c++) {
+    for (int i = 0; i < 7; i++) {
+      for (int j = 0; j < 5; j++) {
+        a[i][j + posx] = font5x7[text[c] - 32][i] & (1 << (4 - j));
+      }
+    }
+    posx += 8;
   }
-  if(s==60) { s=0; m++; } 
-  if(m==60) { m=0; g++; } 
-  if(g==24) { g=0; }
-  
-  draw8x16(0,0,g/10);
-  draw8x16(0,9,g%10);
-  draw8x16(0,23,m/10);
-  draw8x16(0,32,m%10);
+
+  posx = 0;
+
+  while (posx < 40 + (6 * text.length())) {
+    if (posx < 6 * text.length()) {
+      for (int i = 0; i < 7; i++) {
+        FrameBuffer[4 + i][39] = a[i][posx];
+      }
+    }
+    redisplay();
+    posx++;
+    delay(100);
+    for (int i = 0; i < 7; i++) {
+      for (int j = 1; j < 40; j++) {
+        FrameBuffer[i][j - 1] = FrameBuffer[i][j];
+      }
+    }
+  }
+
+  for (int i = 0; i < 7; i++)
+    free(a[i]);
+  free(a);
+  clearBuffer();
+}
+
+void loop() {
+  if (millis() - last >= 300000) {
+    getNTPTime();
+  }
+
+  if (millis() - lastSecond >= 1000) {
+    s++;
+    lastSecond = millis();
+  }
+  if (s == 60) {
+    s = 0;
+    m++;
+  }
+  if (m == 60) {
+    m = 0;
+    g++;
+  }
+  if (g == 24) {
+    g = 0;
+  }
+
+  draw8x16(0, 0, g / 10);
+  draw8x16(0, 9, g % 10);
+  draw8x16(0, 23, m / 10);
+  draw8x16(0, 32, m % 10);
 
   FrameBuffer[5][20] = true; FrameBuffer[6][20] = true; FrameBuffer[5][19] = true; FrameBuffer[6][19] = true;
   FrameBuffer[9][20] = true; FrameBuffer[10][20] = true; FrameBuffer[9][19] = true; FrameBuffer[10][19] = true;
-  
+
   redisplay();
-  delay(1000);
+
+  //----------------WEBSERVER-------------------
+  server.handleClient();
+
+  delay(100);
 }
--- wifi.h
+++ wifi.h
@@ -1,63 +1,68 @@
-#include "WiFiEspUdp.h"
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <ESP8266WebServer.h>
 
-#include "SoftwareSerial.h"
-SoftwareSerial ESP(2,3); // RX, TX
-
-char timeServer[] = "time.nist.gov";
+char ssid[] = "Nigdy wiecej darmowego Wi-Fi";
+char pass[] = "Hulajnoga1999";
 unsigned int localPort = 2390;
+IPAddress timeServerIP;
+const char* ntpServerName = "time.nist.gov";
 const int NTP_PACKET_SIZE = 48;
-const int UDP_TIMEOUT = 2000;
-byte packetBuffer[NTP_PACKET_SIZE];
-
-WiFiEspUDP Udp;
+byte packetBuffer[ NTP_PACKET_SIZE];
+WiFiUDP udp;
+ESP8266WebServer server(80);
 
 int g = 0, m = 0, s = 0;
 long int last = 0;
 
-boolean wyslij(char * Komenda_AT, char *Odpowiedz_AT, int czas_czekania) {
-  ESP.println(Komenda_AT);
-  delay(czas_czekania);
-  while (ESP.available() > 0) {
-    if (ESP.find(Odpowiedz_AT)) {
-      return 1;
-    }
-  }
-  return 0;
-}
+const char INDEX_HTML[] =
+"<!DOCTYPE HTML>"
+"<html>"
+  "<head>"
+    "<meta name = \"viewport\" content = \"width = device-width, initial-scale = 1.0, maximum-scale = 1.0, user-scalable=0\">"
+    "<meta charset=\"utf-8\">"
+    "<title>Sterownik LED</title>"
+  "</head>"
+  "<body>"
+    "<h1>Sterownik zegara LED</h1>"
+    "<FORM action=\"/\" method=\"post\">"
+      "<P>"
+        "Jasność:<br>"
+        "<input type=\"text\" name=\"LEDIntensity\">"
+        "<INPUT type=\"submit\" value=\"Send\">"
+      "</P>"
+    "</FORM>"
+  "</body>"
+"</html>";
 
 void connectWiFi() {
-  while (!wyslij("AT", "OK", 100)) {
-    Serial.println("Błąd komunikacji z modułem");
-    delay(1000);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-  Serial.println("Komunikacja OK");
+  Serial.println("");
 
-  if (wyslij("AT+CWMODE=1", "OK", 500))
-    Serial.println("CWMODE - OK!");
-  if (wyslij("AT+CIPMODE=0", "OK", 500))
-    Serial.println("CIPMODE - OK!");
-  if (wyslij("AT+CIPMUX=1", "OK", 500))
-    Serial.println("CIPMUX - OK!");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
-  if (wyslij("AT+CWJAP=\"Nigdy wiecej darmowego Wi-Fi\",\"Hulajnoga1999\"", "OK", 8000))
-    Serial.println("Polaczono z siecia!");
-  else
-    Serial.println("Błąd łączenia z siecią!");
-
-  if (wyslij("AT+CIPSERVER=1,80", "OK", 5000))
-    Serial.println("Uruchomiono serwer :)");
-
-  ESP.println("AT+CIFSR");
-  Serial.println(ESP.readString());
-  delay(200);  
+  Serial.println("Starting UDP");
+  udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(udp.localPort());
 }
 
-void sendNTPpacket(char *ntpSrv) {
+unsigned long sendNTPpacket(IPAddress& address)
+{
+  Serial.println("sending NTP packet...");
   // set all bytes in the buffer to 0
   memset(packetBuffer, 0, NTP_PACKET_SIZE);
   // Initialize values needed to form NTP request
   // (see URL above for details on the packets)
-
   packetBuffer[0] = 0b11100011;   // LI, Version, Mode
   packetBuffer[1] = 0;     // Stratum, or type of clock
   packetBuffer[2] = 6;     // Polling Interval
@@ -70,29 +75,46 @@ void sendNTPpacket(char *ntpSrv) {
 
   // all NTP fields have been given values, now
   // you can send a packet requesting a timestamp:
-  Udp.beginPacket(ntpSrv, 123); //NTP requests are to port 123
-
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-
-  Udp.endPacket();
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
 }
 
 void getNTPTime() {
-  sendNTPpacket(timeServer);
+  if (WiFi.status() != WL_CONNECTED) connectWiFi();
   
-  unsigned long startMs = millis();
-  while (!Udp.available() && (millis() - startMs) < UDP_TIMEOUT) {}
+  //get a random server from the pool
+  WiFi.hostByName(ntpServerName, timeServerIP); 
 
-  Serial.println(Udp.parsePacket());
-  if (Udp.parsePacket()) {
-    Serial.println("packet received");
-
-    Udp.read(packetBuffer, NTP_PACKET_SIZE);
+  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+  // wait to see if a reply is available
+  delay(1000);
+  
+  int cb = udp.parsePacket();
+  if (!cb) {
+    Serial.println("no packet yet");
+  }
+  else {
+    Serial.print("packet received, length=");
+    Serial.println(cb);
+    // We've received a packet, read the data from it
+    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
     unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
     unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
     unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = " );
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
     const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
     unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
 
     g = (((epoch  % 86400L) / 3600) + 2) % 24;
     m = (epoch  % 3600) / 60;
@@ -113,4 +135,19 @@ void getNTPTime() {
   }
 
   last = millis();
+}
+
+void handleRoot() {    
+  if (server.hasArg("LEDIntensity")) {
+    String LEDIntensity = server.arg("LEDIntensity");
+    Serial.println(LEDIntensity);
+    int ledI = LEDIntensity.toInt();
+    EEPROM.write(0, ledI);
+    EEPROM.commit();
+    lmd.setIntensity(EEPROM.read(0));
+    server.send(200, "text/html", INDEX_HTML);
+  }
+  else {
+    server.send(200, "text/html", INDEX_HTML);
+  }
 }
