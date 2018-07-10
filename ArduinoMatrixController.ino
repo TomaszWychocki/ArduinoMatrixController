5826a6cc5b1f7a8190266ed39c89dc2c60d0e2c5
Tomasz Wychocki <wychoniu@gmail.com>
2018-07-10 20:25:37 +0200

    Usunieto problemy z dziwnym zachowaniem wyswietlacza

--- ArduinoMatrixController.ino
+++ ArduinoMatrixController.ino
@@ -1,7 +1,8 @@
 #include <MD_MAX72xx.h>
+#include <avr/pgmspace.h>
 #include <SPI.h>
 #include <Wire.h>
-#include "RTClib.h"
+#include <RTClib.h>
 #include "font.h"
 
 #define MODULES_X 5
@@ -19,13 +20,13 @@ MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 RTC_DS1307 RTC;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 bool FrameBuffer[PIXELS_Y][PIXELS_X];
-long int lastSecond = 0;
-int g = 0, m = 0, s = 40;
+uint8_t g = 0, m = 0, s = 0;
+DateTime now;
 
 void clearBuffer()
 {
-  for (int i = 0; i < PIXELS_Y; i++)
-    for (int j = 0; j < PIXELS_X; j++)
+  for (size_t i = 0; i < PIXELS_Y; i++)
+    for (size_t j = 0; j < PIXELS_X; j++)
       FrameBuffer[i][j] = false;
 }
 
@@ -36,112 +37,72 @@ void resetMatrix(void)
   mx.clear();
 }
 
-void setup() {
+void setup()
+{
   clearBuffer();
   mx.begin();
-  resetMatrix(); 
+  resetMatrix();
   Wire.begin();
-  RTC.begin(); 
-  draw8x16(0, 0, g / 10);
-  draw8x16(0, 9, g % 10);
-  draw8x16(0, 23, m / 10);
-  draw8x16(0, 32, m % 10);
-  redisplay();
+  RTC.begin();
+  //RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
-void redisplay() {
+void redisplay()
+{
   //mx.clear();
   mx.update(MD_MAX72XX::OFF);
 
-  for (int i = 0; i < PIXELS_Y / 2; i++) {
-    for (int j = 0; j < PIXELS_X; j++) {
+  for (size_t i = 0; i < PIXELS_Y / 2; i++)
+  {
+    for (size_t j = 0; j < PIXELS_X; j++)
+    {
       mx.setPoint(i, PIXELS_X - j - 1, FrameBuffer[i][j]);
     }
   }
 
-  for (int i = PIXELS_Y / 2; i < PIXELS_Y; i++) {
-    for (int j = 0; j < PIXELS_X; j++) {
+  for (size_t i = PIXELS_Y / 2; i < PIXELS_Y; i++)
+  {
+    for (size_t j = 0; j < PIXELS_X; j++)
+    {
       mx.setPoint(i - (PIXELS_Y / 2), PIXELS_X - j + 39, FrameBuffer[i][j]);
     }
   }
-  
+
   mx.update(MD_MAX72XX::ON);
 }
 
-void draw8x16(int posY, int posX, int number) {
-  for (int i = 0; i < 16; i++) {
-    for (int j = 0; j < 8; j++) {
-      FrameBuffer[posY + i][posX + j] = font8x16[number][i] & (1 << (7 - j));
+void draw8x16(size_t posY, size_t posX, uint8_t number)
+{
+  if(number > 9 || number < 0) number = 0;
+  
+  for (size_t i = 0; i < 16; i++)
+  {
+    for (size_t j = 0; j < 8; j++)
+    {
+      FrameBuffer[posY + i][posX + j] = pgm_read_byte(&(font8x16[number][i])) & (1 << (7 - j));
     }
   }
 }
 
-void displayText(String text, int speed) {
-  bool **a;
-  a = (bool **) malloc(7 * sizeof(bool *));
-  for (int i = 0; i < 7; i++)
-    a[i] = (bool *) malloc(7 * text.length() * sizeof(bool));
+void loop()
+{
+  now = RTC.now();
 
-  for (int i = 0; i < 7; i++)
-    for (int j = 0; j < (7 * text.length()); j++)
-      a[i][j] = false;
-
-  clearBuffer();
-  int posx = 0;
-
-  for (int c = 0; c < text.length(); c++) {
-    int t = text[c] - 32;
-    if (t >= 65) t--;
-    for (int i = 0; i < 7; i++) {
-      for (int j = 0; j < 5; j++) {
-        a[i][j + posx] = font5x7[t][i] & (1 << (4 - j));
-      }
-    }
-    posx += 6;
-  }
-
-  posx = 0;
-
-  while (posx < (39 + (6 * text.length()))) {
-    if (posx < 6 * text.length()) {
-      for (int i = 0; i < 7; i++) {
-        FrameBuffer[4 + i][39] = a[i][posx];
-      }
-    }
-    redisplay();
-    posx++;
-    delay(speed);
-    for (int i = 0; i < 7; i++) {
-      for (int j = 1; j < 40; j++) {
-        FrameBuffer[4 + i][j - 1] = FrameBuffer[4 + i][j];
-      }
-    }
-  }
-
-  for (int i = 0; i < 7; i++)
-    free(a[i]);
-  free(a);
-  clearBuffer();
-}
-
-void loop() {
-  if (millis() - lastSecond >= 1000) {
-    lastSecond = millis();
-
-    DateTime now = RTC.now();
+  if (now.second() != s)
+  {
     g = now.hour();
     m = now.minute();
-
+    s = now.second();
+    draw8x16(0, 0, (g / 10) % 10);
+    draw8x16(0, 9, g % 10);
+    draw8x16(0, 23, (m / 10) % 10);
+    draw8x16(0, 32, m % 10);
+  
+    FrameBuffer[5][20] = true; FrameBuffer[6][20] = true; FrameBuffer[5][19] = true; FrameBuffer[6][19] = true;
+    FrameBuffer[9][20] = true; FrameBuffer[10][20] = true; FrameBuffer[9][19] = true; FrameBuffer[10][19] = true;
+  
     redisplay();
     clearBuffer();
   }
-
-  draw8x16(0, 0, g / 10);
-  draw8x16(0, 9, g % 10);
-  draw8x16(0, 23, m / 10);
-  draw8x16(0, 32, m % 10);
-
-  FrameBuffer[5][20] = true; FrameBuffer[6][20] = true; FrameBuffer[5][19] = true; FrameBuffer[6][19] = true;
-  FrameBuffer[9][20] = true; FrameBuffer[10][20] = true; FrameBuffer[9][19] = true; FrameBuffer[10][19] = true;
 }
 
